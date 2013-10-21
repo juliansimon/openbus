dcd0f0e0d0089d48dc02636beb3249270c1766c3
Javier Alba <me@fjavieralba.com>
2013-10-21 12:56:18 +0200

    Added license text. Removed unused methods. Now passing tests

--- broker/java/test/com/produban/openbus/broker/producer/BasicProducerTest.java
+++ broker/java/test/com/produban/openbus/broker/producer/BasicProducerTest.java
@@ -1,9 +1,24 @@
+/*
+ * Copyright 2013 Produban
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.produban.openbus.broker.producer;
 
 import static org.junit.Assert.*;
 import kafka.message.MessageAndMetadata;
 
-import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -39,16 +54,6 @@ public class BasicProducerTest {
 		
 		MessageAndMetadata<byte[], byte[]> secondMessage = consumer.consumeOne();
 		assertEquals("second message", secondMessage.message());
-
-	}
-
-	@Test
-	public void sendRandomLogEvents() {
-		fail("ouch!");
-	}
-
-	@AfterClass
-	public static void stopKafka(){
 
 	}
 
